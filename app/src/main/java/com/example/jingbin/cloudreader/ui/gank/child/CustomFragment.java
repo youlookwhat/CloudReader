d642faf791fff34403f53725a2857d55d74bd4d0
youlookwhat <770413277@qq.com>
2016-12-14 17:57:28 +0800

    v1.0 干货订制增加bottomsheet显示选择菜单

--- app/src/main/java/com/example/jingbin/cloudreader/ui/gank/child/CustomFragment.java
+++ app/src/main/java/com/example/jingbin/cloudreader/ui/gank/child/CustomFragment.java
@@ -1,10 +1,14 @@
 package com.example.jingbin.cloudreader.ui.gank.child;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.cocosw.bottomsheet.BottomSheet;
 import com.example.jingbin.cloudreader.R;
 import com.example.jingbin.cloudreader.adapter.AndroidAdapter;
 import com.example.jingbin.cloudreader.app.Constants;
@@ -14,6 +18,7 @@ import com.example.jingbin.cloudreader.databinding.FragmentCustomBinding;
 import com.example.jingbin.cloudreader.http.HttpUtils;
 import com.example.jingbin.cloudreader.http.cache.ACache;
 import com.example.jingbin.cloudreader.utils.DebugUtil;
+import com.example.jingbin.cloudreader.utils.SPUtils;
 import com.example.xrecyclerview.XRecyclerView;
 
 import rx.Observer;
@@ -105,7 +110,7 @@ public class CustomFragment extends BaseFragment<FragmentCustomBinding> {
 
                                 mACache.remove(Constants.GANK_CUSTOM);
                                 // 缓存50分钟
-                                mACache.put(Constants.GANK_CUSTOM, gankIoDataBean, 3);
+                                mACache.put(Constants.GANK_CUSTOM, gankIoDataBean, 30000);
                             }
                         } else {
                             if (gankIoDataBean != null && gankIoDataBean.getResults() != null && gankIoDataBean.getResults().size() > 0) {
@@ -128,13 +133,7 @@ public class CustomFragment extends BaseFragment<FragmentCustomBinding> {
 
         if (mHeaderView == null) {
             mHeaderView = View.inflate(getContext(), R.layout.header_item_gank_custom, null);
-            View view = mHeaderView.findViewById(R.id.ll_choose_catalogue);
-            view.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                }
-            });
+            initHeader(mHeaderView);
             bindingView.xrvCustom.addHeaderView(mHeaderView);
         }
 
@@ -145,6 +144,102 @@ public class CustomFragment extends BaseFragment<FragmentCustomBinding> {
         mAndroidAdapter.notifyDataSetChanged();
 
         mIsFirst = false;
+    }
+
+    private void initHeader(View mHeaderView) {
+        final TextView txName = (TextView) mHeaderView.findViewById(R.id.tx_name);
+        String gankCala = SPUtils.getString("gank_cala", "全部");
+        txName.setText(gankCala);
+//        setTypeName(txName);
+        View view = mHeaderView.findViewById(R.id.ll_choose_catalogue);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                new BottomSheet.Builder(getActivity(), R.style.BottomSheet_StyleDialog).title("选择分类").sheet(R.menu.gank_bottomsheet).listener(new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case R.id.gank_all:
+                                if (isOtherType(v, "全部")) {
+                                    txName.setText("全部");
+                                    mType = "all";// 全部传 all
+                                    SPUtils.putString("gank_cala", "全部");
+                                    showLoading();
+                                    loadCustomData();
+                                }
+                                break;
+                            case R.id.gank_ios:
+                                if (isOtherType(v, "IOS")) {
+                                    txName.setText("IOS");
+                                    mType = "iOS";// 这里有严格大小写
+                                    SPUtils.putString("gank_cala", "IOS");
+                                    showLoading();
+                                    loadCustomData();
+                                }
+                                break;
+                            case R.id.gank_qian:
+                                if (isOtherType(v, "前端")) {
+                                    changeContent(txName, "前端");
+                                }
+                                break;
+                            case R.id.gank_movie:
+                                if (isOtherType(v, "休息视频")) {
+                                    changeContent(txName, "休息视频");
+                                }
+                                break;
+                            case R.id.gank_resouce:
+                                if (isOtherType(v, "拓展资源")) {
+                                    changeContent(txName, "拓展资源");
+                                }
+                                break;
+                        }
+                    }
+                }).show();
+
+            }
+        });
+    }
+
+    private void changeContent(TextView textView, String content) {
+        textView.setText(content);
+        mType = content;
+        SPUtils.putString("gank_cala", content);
+        showLoading();
+        loadCustomData();
+    }
+
+    private boolean isOtherType(View view, String selectType) {
+        String clickText = SPUtils.getString("gank_cala", "全部");
+        if (clickText.equals(selectType)) {
+//            Snackbar.make(view, "当前已经是" + selectType + "分类", Snackbar.LENGTH_SHORT).show();
+            Toast.makeText(view.getContext(), "当前已经是" + selectType + "分类", Toast.LENGTH_LONG).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void setTypeName(TextView txName) {
+        String gankCala = SPUtils.getString("gank_cala", "全部");
+        txName.setText(gankCala);
+        switch (gankCala) {
+            case "全部":
+                txName.setText("全部");
+                break;
+            case "IOS":
+                txName.setText("IOS");
+                break;
+            case "前端":
+                txName.setText("前端");
+                break;
+            case "休息视频":
+                txName.setText("休息视频");
+                break;
+            case "拓展资源":
+                txName.setText("拓展资源");
+                break;
+
+        }
     }
 
     /**
