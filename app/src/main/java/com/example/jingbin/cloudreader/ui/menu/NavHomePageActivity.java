90e6ec0c88e9ab8b55c7e51b074a3e2f770ff292
youlookwhat <770413277@qq.com>
2016-12-29 00:22:13 +0800

    v1.0 增加分享，过渡图显示优化

--- app/src/main/java/com/example/jingbin/cloudreader/ui/menu/NavHomePageActivity.java
+++ app/src/main/java/com/example/jingbin/cloudreader/ui/menu/NavHomePageActivity.java
@@ -5,10 +5,12 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.jingbin.cloudreader.R;
 import com.example.jingbin.cloudreader.databinding.ActivityNavHomePageBinding;
+import com.example.jingbin.cloudreader.utils.ShareUtils;
 import com.example.jingbin.cloudreader.view.statusbar.StatusBarUtil;
 
 public class NavHomePageActivity extends AppCompatActivity {
@@ -20,9 +22,16 @@ public class NavHomePageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_nav_home_page);
 
+        binding.toolbarLayout.setTitle(getString(R.string.app_name));
         initTranslucentBar();
-
+        binding.fabShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShareUtils.share(v.getContext(), R.string.string_share_text);
+            }
+        });
     }
+
 
     private void initTranslucentBar() {
         StatusBarUtil.setTranslucentForImageView(this, 0, binding.toolbarLayout);
@@ -36,8 +45,5 @@ public class NavHomePageActivity extends AppCompatActivity {
     public static void startHome(Context mContext) {
         Intent intent = new Intent(mContext, NavHomePageActivity.class);
         mContext.startActivity(intent);
-
-//        android:text="@string/string_about"
-//        android:textColor="@color/colorTitle"
     }
 }
