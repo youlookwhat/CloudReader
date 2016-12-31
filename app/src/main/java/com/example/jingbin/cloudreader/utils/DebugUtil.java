7266f739d032d30cebd47101caae93067f2e3699
youlookwhat <770413277@qq.com>
2017-01-01 00:58:33 +0800

    v1.0 close log

--- app/src/main/java/com/example/jingbin/cloudreader/utils/DebugUtil.java
+++ app/src/main/java/com/example/jingbin/cloudreader/utils/DebugUtil.java
@@ -7,7 +7,7 @@ import android.widget.Toast;
 //在代码中要打印log,就直接DebugUtil.debug(....).然后如果发布的时候,就直接把这个类的DEBUG 改成false,这样所有的log就不会再打印在控制台.
 public class DebugUtil {
     public static final String TAG = "jingbin";
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
 
     public static void toast(Context context, String content) {
         Toast.makeText(context, content, Toast.LENGTH_SHORT).show();
