f3bfdda334a57167b44ea6892ffcb24054f5257c
youlookwhat <770413277@qq.com>
2018-09-29 09:56:26 +0800

    修复：修复webview视频全屏的问题

--- app/src/main/java/com/example/jingbin/cloudreader/ui/wan/child/MyCollectActivity.java
+++ app/src/main/java/com/example/jingbin/cloudreader/ui/wan/child/MyCollectActivity.java
@@ -3,7 +3,6 @@ package com.example.jingbin.cloudreader.ui.wan.child;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 
 import com.example.jingbin.cloudreader.R;
@@ -35,7 +34,6 @@ public class MyCollectActivity extends BaseActivity<ActivityMyCollectBinding> {
         MyFragmentPagerAdapter myAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager(), mFragments, mTitleList);
         bindingView.vpMyCollect.setAdapter(myAdapter);
         myAdapter.notifyDataSetChanged();
-        bindingView.tabMyCollect.setTabMode(TabLayout.MODE_SCROLLABLE);
         bindingView.tabMyCollect.setupWithViewPager(bindingView.vpMyCollect);
         showContentView();
     }
