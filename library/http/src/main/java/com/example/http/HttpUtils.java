9e5a1d64c3459cae5f9b93d27d8d58a8d558deda
jingbin <770413277@qq.com>
2019-03-13 09:30:10 +0800

    wanandroid change https

--- library/http/src/main/java/com/example/http/HttpUtils.java
+++ library/http/src/main/java/com/example/http/HttpUtils.java
@@ -64,7 +64,7 @@ public class HttpUtils {
     public final static String API_DOUBAN = "Https://api.douban.com/";
     public final static String API_TING = "https://tingapi.ting.baidu.com/v1/restserver/";
     public final static String API_FIR = "http://api.fir.im/apps/";
-    public final static String API_WAN_ANDROID = "http://www.wanandroid.com/";
+    public final static String API_WAN_ANDROID = "https://www.wanandroid.com/";
     public final static String API_QSBK = "http://m2.qiushibaike.com/";
     /**
      * 分页数据，每页的数量
