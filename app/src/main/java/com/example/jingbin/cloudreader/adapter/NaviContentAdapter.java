29c27db0077d7b7270bb0600c29b1228a1f39102
youlookwhat <770413277@qq.com>
2019-06-17 23:18:44 +0800

    fix: use FlowLayout replace TagFlowLayout

--- app/src/main/java/com/example/jingbin/cloudreader/adapter/NaviContentAdapter.java
+++ app/src/main/java/com/example/jingbin/cloudreader/adapter/NaviContentAdapter.java
@@ -1,13 +1,9 @@
 package com.example.jingbin.cloudreader.adapter;
 
-import android.content.Context;
-import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.text.Html;
-import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.jingbin.cloudreader.R;
@@ -17,11 +13,7 @@ import com.example.jingbin.cloudreader.bean.wanandroid.ArticlesBean;
 import com.example.jingbin.cloudreader.bean.wanandroid.NaviJsonBean;
 import com.example.jingbin.cloudreader.databinding.ItemNaviContentBinding;
 import com.example.jingbin.cloudreader.utils.CommonUtils;
-import com.example.jingbin.cloudreader.utils.DensityUtil;
 import com.example.jingbin.cloudreader.view.webview.WebViewActivity;
-import com.zhy.view.flowlayout.FlowLayout;
-import com.zhy.view.flowlayout.TagAdapter;
-import com.zhy.view.flowlayout.TagFlowLayout;
 
 import java.util.List;
 
@@ -53,24 +45,21 @@ public class NaviContentAdapter extends BaseRecyclerViewAdapter<NaviJsonBean.Dat
         }
     }
 
-    private void showTagView(TagFlowLayout flowlayoutHot, final List<ArticlesBean> beanList) {
-        flowlayoutHot.removeAllViews();
-        flowlayoutHot.setAdapter(new TagAdapter<ArticlesBean>(beanList) {
-            @Override
-            public View getView(FlowLayout parent, int position, ArticlesBean bean) {
-                TextView textView = (TextView) View.inflate(parent.getContext(), R.layout.layout_navi_tag, null);
-                textView.setTextColor(CommonUtils.randomColor());
-                textView.setText(Html.fromHtml(bean.getTitle()));
-                return textView;
-            }
-        });
-        flowlayoutHot.setOnTagClickListener(new TagFlowLayout.OnTagClickListener() {
-            @Override
-            public boolean onTagClick(View view, int position, FlowLayout parent) {
-                WebViewActivity.loadUrl(view.getContext(), beanList.get(position).getLink(), beanList.get(position).getTitle());
-                return true;
-            }
-        });
+    private void showTagView(android.support.design.internal.FlowLayout flowLayout, final List<ArticlesBean> beanList) {
+        flowLayout.removeAllViews();
+        for (int i = 0; i < beanList.size(); i++) {
+            TextView textView = (TextView) View.inflate(flowLayout.getContext(), R.layout.layout_navi_tag, null);
+            textView.setTextColor(CommonUtils.randomColor());
+            textView.setText(Html.fromHtml(beanList.get(i).getTitle()));
+            flowLayout.addView(textView);
+            int finalI = i;
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    WebViewActivity.loadUrl(view.getContext(), beanList.get(finalI).getLink(), beanList.get(finalI).getTitle());
+                }
+            });
+        }
     }
 
 }
