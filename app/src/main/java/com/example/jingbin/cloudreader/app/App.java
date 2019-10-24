6c48f7a0d9e9b86711ea043dd817469b625bd056
miqt <miqingtang@163.com>
2019-10-24 17:32:07 +0800

    bugly 调好了

--- app/src/main/java/com/example/jingbin/cloudreader/app/App.java
+++ app/src/main/java/com/example/jingbin/cloudreader/app/App.java
@@ -1,10 +1,13 @@
 package com.example.jingbin.cloudreader.app;
 
+import android.content.Context;
 import android.support.multidex.MultiDexApplication;
 
 import com.analysys.track.AnalysysTracker;
 import com.example.http.HttpUtils;
 import com.tencent.bugly.crashreport.CrashReport;
+
+import java.lang.reflect.Method;
 
 /**
  * Created by jingbin on 2016/11/22.
@@ -31,12 +34,44 @@ public class App extends MultiDexApplication {
 
         CrashReport.initCrashReport(getApplicationContext(), "97876e6b1d", true);
         CrashReport.enableBugly(true);
-        CrashReport.postCatchedException(new Exception());
 
         // 设置打开debug模式，上线请置为false
         AnalysysTracker.setDebugMode(this, false);
         // 初始化接口:第二个参数填写您在平台申请的appKey,第三个参数填写
         AnalysysTracker.init(this, "7752552892442721d", "YUNYUE");
+    }
+
+    private static boolean tag = false;
+
+    public static void commitError(Throwable throwable) {
+        try {
+            Class clazz = Class.forName("com.tencent.bugly.crashreport.CrashReport");
+            if (!tag) {
+                setTag(clazz);
+            }
+            postException(throwable, clazz);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void postException(Throwable throwable, Class<?> clazz) {
+        try {
+            Method postCatchedException = clazz.getMethod("postCatchedException", Throwable.class);
+            postCatchedException.invoke(null, throwable);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void setTag(Class<?> clazz) {
+        try {
+            Method setUserSceneTag = clazz.getMethod("setUserSceneTag", Context.class, int.class);
+            setUserSceneTag.invoke(null, App.getInstance(), 9999);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        tag = true;
     }
 
 }
