3640d09b31be1b9ebe2cec5c2d8c605ebf51b873
jingbin <770413277@qq.com>
2020-10-26 16:50:22 +0800

    add category adapter market ratings

--- app/src/main/java/com/example/jingbin/cloudreader/adapter/CategoryArticleAdapter.java
+++ app/src/main/java/com/example/jingbin/cloudreader/adapter/CategoryArticleAdapter.java
@@ -1,14 +1,19 @@
 package com.example.jingbin.cloudreader.adapter;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.view.View;
 
 import com.example.jingbin.cloudreader.R;
+import com.example.jingbin.cloudreader.app.Constants;
 import com.example.jingbin.cloudreader.bean.wanandroid.ArticlesBean;
 import com.example.jingbin.cloudreader.data.UserUtil;
 import com.example.jingbin.cloudreader.data.model.CollectModel;
 import com.example.jingbin.cloudreader.databinding.ItemCategoryArticleBinding;
+import com.example.jingbin.cloudreader.utils.BaseTools;
+import com.example.jingbin.cloudreader.utils.DialogBuild;
 import com.example.jingbin.cloudreader.utils.PerfectClickListener;
+import com.example.jingbin.cloudreader.utils.SPUtils;
 import com.example.jingbin.cloudreader.utils.ToastUtil;
 import com.example.jingbin.cloudreader.ui.WebViewActivity;
 import com.example.jingbin.cloudreader.viewmodel.wan.WanNavigator;
@@ -37,9 +42,9 @@ public class CategoryArticleAdapter extends BaseBindingAdapter<ArticlesBean, Ite
             binding.setAdapter(this);
             binding.setBean(bean);
             binding.executePendingBindings();
-            binding.vbCollect.setOnClickListener(new PerfectClickListener() {
+            binding.vbCollect.setOnClickListener(new View.OnClickListener() {
                 @Override
-                protected void onNoDoubleClick(View v) {
+                public void onClick(View v) {
                     if (UserUtil.isLogin(activity) && model != null) {
                         if (!binding.vbCollect.isChecked()) {
                             model.unCollect(false, bean.getId(), bean.getOriginId(), new WanNavigator.OnCollectNavigator() {
@@ -61,10 +66,29 @@ public class CategoryArticleAdapter extends BaseBindingAdapter<ArticlesBean, Ite
                                 @Override
                                 public void onSuccess() {
                                     bean.setCollect(true);
+                                    ToastUtil.showToastLong("收藏成功");
+                                    v.postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            if (BaseTools.isApplicationAvilible(activity, "com.coolapk.market")) {
+                                                boolean show = SPUtils.getBoolean(Constants.SHOW_MARKET, false);
+                                                if (!show) {
+                                                    SPUtils.putBoolean(Constants.SHOW_MARKET, true);
+                                                    DialogBuild.showCustom(v, 1, "很高兴你使用云阅，如果用的愉快的话可以去酷安支持一下哦~", "去支持", "取消", new DialogInterface.OnClickListener() {
+                                                        @Override
+                                                        public void onClick(DialogInterface dialog, int which) {
+                                                            BaseTools.launchAppDetail(activity, "com.example.jingbin.cloudreader", "com.coolapk.market");
+                                                        }
+                                                    });
+                                                }
+                                            }
+                                        }
+                                    }, 200);
                                 }
 
                                 @Override
                                 public void onFailure() {
+                                    ToastUtil.showToastLong("收藏失败");
                                     bean.setCollect(false);
                                     refreshNotifyItemChanged(position);
                                 }
